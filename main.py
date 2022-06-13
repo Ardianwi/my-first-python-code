86dc96fae758c155b1d04a5305a5c774d798f182
DESIGN <ardianwicaksono2021@gmail.com>
2022-06-13 21:19:30 +0700

    Budi beli telur dan susu

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 """
-ini adalah demo project pertama dengan python
+Semua sintaksis dasar Bahasa pemrograman terdiri dari:
+1. sekuensial: langkah berurutan
+2. Percabangan: langkah melompat jika kondisi terpenuhi
+3. Perulangan: mengulang langkah yang semua berkali-kali selama/sampai kondisi terpenuhi
 """
 
-print("hello world")
-print("hello world")
+# Sekuensial
+print('ibu menyuruh"pergi ke toko membeli telur dan susu"')
+print('Budi menjawab,"Baik, apa yang saya lakukan di toko?"')
+print('ibu menjawa, "Beli 1 botol susu, dan jika ada telur 6"')
+print('Maka Budi berangkat sekarang')
+print('Budi mulai berelanja')
